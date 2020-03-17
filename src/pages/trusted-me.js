03b5f736c2add24c7320a5ec0d669f50e38f15df
Samuel <samukedo@gmail.com>
2020-03-17 19:53:48 +0100

    trusted-me done

--- src/pages/trusted-me.js
+++ src/pages/trusted-me.js
@@ -1,64 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
-const TrustedMe = () => (
-	<section className="content">
-		<h1>Trusted Me</h1>
-		<p style={{ padding: '3rem' }}>
-			Id aliqua id nisi ipsum officia sunt aute adipisicing ut ipsum labore officia nulla. Irure ullamco ullamco
-			duis veniam ad incididunt id ipsum. Non aliquip pariatur ad sunt. Elit eiusmod do elit non aliquip
-			reprehenderit laboris. Minim cupidatat pariatur ut quis amet tempor adipisicing qui occaecat aliquip est
-			adipisicing. Nulla reprehenderit Lorem ipsum minim nisi fugiat. Et cupidatat aliquip cupidatat aute aliquip
-			esse mollit incididunt excepteur consequat incididunt eiusmod culpa. Irure consequat labore magna laboris
-			Lorem laborum occaecat. Reprehenderit magna fugiat veniam cillum cillum exercitation reprehenderit aliquip.
-			Aliquip ea nostrud sunt ex cillum. Ullamco amet consectetur duis sunt tempor ullamco nostrud fugiat irure.
-			Cupidatat et irure ex minim magna elit ex sit exercitation eu consectetur culpa ullamco. Sint incididunt et
-			eiusmod minim sunt fugiat cupidatat sint ex ex voluptate sunt laborum nostrud. Quis commodo dolore labore
-			laboris reprehenderit velit amet excepteur nulla. In eu commodo sunt excepteur eu culpa eiusmod ut ullamco
-			cillum anim elit Lorem laboris. Aute laboris ad consectetur fugiat Lorem. Lorem sint aliqua magna sunt
-			cillum anim elit Lorem laboris. Aute laboris ad consectetur fugiat Lorem. Lorem sint aliqua magna sunt
-			cillum anim elit Lorem laboris. Aute laboris ad consectetur fugiat Lorem. Lorem sint aliqua magna sunt
-			cillum anim elit Lorem laboris. Aute laboris ad consectetur fugiat Lorem. Lorem sint aliqua magna sunt
-			cillum anim elit Lorem laboris. Aute laboris ad consectetur fugiat Lorem. Lorem sint aliqua magna sunt
-			cillum anim elit Lorem laboris. Aute laboris ad consectetur fugiat Lorem. Lorem sint aliqua magna sunt
-			cillum anim elit Lorem laboris. Aute laboris ad consectetur fugiat Lorem. Lorem sint aliqua magna sunt
-			ipsum. Aute proident elit commodo nostrud consequat aliqua duis mollit veniam. Laborum aute Lorem ut dolore
-			minim commodo cillum enim cupidatat. Eiusmod dolor consectetur irure eiusmod non id laborum dolor anim est
-			ut consectetur. Tempor dolor sint quis eiusmod ipsum consequat ad labore in. Culpa aliqua velit id qui eu
-			sunt ex fugiat duis id. Anim et laborum nostrud sit quis et excepteur enim cupidatat ad ipsum. Commodo
-			laboris nulla tempor eu eiusmod voluptate sit elit pariatur laboris pariatur ipsum sunt laborum. Deserunt
-			veniam aliquip ipsum fugiat eiusmod. In et quis anim voluptate et veniam dolor reprehenderit sit sunt
-			consectetur mollit esse. Incididunt esse labore commodo excepteur quis eiusmod veniam officia magna dolore
-			in tempor. Amet cupidatat culpa incididunt officia enim. Qui laboris adipisicing officia mollit minim ea
-			occaecat dolore. Enim magna fugiat nulla Lorem sunt proident aute culpa nulla. Ad eiusmod elit incididunt
-			sint non officia cupidatat. cillum anim elit Lorem laboris. Aute laboris ad consectetur fugiat Lorem. Lorem
-			sint aliqua magna sunt cillum anim elit Lorem laboris. Aute laboris ad consectetur fugiat Lorem. Lorem sint
-			aliqua magna sunt cillum anim elit Lorem laboris. Aute laboris ad consectetur fugiat Lorem. Lorem sint
-			aliqua magna sunt cillum anim elit Lorem laboris. Aute laboris ad consectetur fugiat Lorem. Lorem sint
-			aliqua magna sunt ipsum. Aute proident elit commodo nostrud consequat aliqua duis mollit veniam. Laborum
-			aute Lorem ut dolore minim commodo cillum enim cupidatat. Eiusmod dolor consectetur irure eiusmod non id
-			laborum dolor anim est ut consectetur. Tempor dolor sint quis eiusmod ipsum consequat ad labore in. Culpa
-			aliqua velit id qui eu sunt ex fugiat duis id. Anim et laborum nostrud sit quis et excepteur enim cupidatat
-			ad ipsum. Commodo laboris nulla tempor eu eiusmod voluptate sit elit pariatur laboris pariatur ipsum sunt
-			laborum. Deserunt veniam aliquip ipsum fugiat eiusmod. In et quis anim voluptate et veniam dolor
-			reprehenderit sit sunt consectetur mollit esse. Incididunt esse labore commodo excepteur quis eiusmod veniam
-			officia magna dolore in tempor. Amet cupidatat culpa incididunt officia enim. Qui laboris adipisicing
-			officia mollit minim ea occaecat dolore. Enim magna fugiat nulla Lorem sunt proident aute culpa nulla. Ad
-			eiusmod elit incididunt sint non officia cupidatat. cillum anim elit Lorem laboris. Aute laboris ad
-			consectetur fugiat Lorem. Lorem sint aliqua magna sunt cillum anim elit Lorem laboris. Aute laboris ad
-			consectetur fugiat Lorem. Lorem sint aliqua magna sunt cillum anim elit Lorem laboris. Aute laboris ad
-			consectetur fugiat Lorem. Lorem sint aliqua magna sunt cillum anim elit Lorem laboris. Aute laboris ad
-			consectetur fugiat Lorem. Lorem sint aliqua magna sunt ipsum. Aute proident elit commodo nostrud consequat
-			aliqua duis mollit veniam. Laborum aute Lorem ut dolore minim commodo cillum enim cupidatat. Eiusmod dolor
-			consectetur irure eiusmod non id laborum dolor anim est ut consectetur. Tempor dolor sint quis eiusmod ipsum
-			consequat ad labore in. Culpa aliqua velit id qui eu sunt ex fugiat duis id. Anim et laborum nostrud sit
-			quis et excepteur enim cupidatat ad ipsum. Commodo laboris nulla tempor eu eiusmod voluptate sit elit
-			pariatur laboris pariatur ipsum sunt laborum. Deserunt veniam aliquip ipsum fugiat eiusmod. In et quis anim
-			voluptate et veniam dolor reprehenderit sit sunt consectetur mollit esse. Incididunt esse labore commodo
-			excepteur quis eiusmod veniam officia magna dolore in tempor. Amet cupidatat culpa incididunt officia enim.
-			Qui laboris adipisicing officia mollit minim ea occaecat dolore. Enim magna fugiat nulla Lorem sunt proident
-			aute culpa nulla. Ad eiusmod elit incididunt sint non officia cupidatat.
-		</p>
-	</section>
-);
+import { LanguageContext } from '../context';
+import Partner from '../components/partner';
+
+import amsMetal from '../assets/partners/ams-metal.png';
+import barkaCieslika from '../assets/partners/barka-cieslika.png';
+import boscoStudio from '../assets/partners/bosco-studio.png';
+import clou from '../assets/partners/clou.png';
+import dha from '../assets/partners/dha.png';
+import kostBet from '../assets/partners/kost-bet.png';
+import projektW from '../assets/partners/projekt-w.png';
+import solidfloor from '../assets/partners/solidfloor.png';
+
+const TrustedMe = () => {
+	const context = useContext(LanguageContext);
+	return (
+		<section className="content">
+			<h1>{context.dictionary.trustedMe.title}</h1>
+			<h4>{context.dictionary.trustedMe.subtitle}:</h4>
+			<div className="partners">
+				<Partner src={barkaCieslika} link="https://www.anta-architekci.pl" alt="Barka Cieślika" style={{padding: ".7rem"}}/>
+				<Partner src={projektW} link="http://www.projektw.pl" alt="Projekt W" />
+				<Partner src={boscoStudio} link="http://www.boscostudio.pl" alt="BOSCO Studio" />
+				<Partner src={clou} link="https://www.clou.nl" alt="clou" style={{padding: "2.5rem"}} />
+				<Partner src={amsMetal} link="http://amsmetal.pl" alt="AMS METAL" />
+				<Partner src={dha} alt="DHa" style={{transform: "scale(1.2)"}}/>
+				<Partner src={kostBet} link="https://kostbet.pl/" alt="KOST-BET" style={{padding: "1rem"}}/>
+				<Partner src={solidfloor} link="https://www.solidfloor.com/en" alt="SOLID floor" style={{padding: "1rem"}}/>
+			</div>
+		</section>
+	);
+};
 
 export default TrustedMe;
