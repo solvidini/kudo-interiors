6c9679757c82ff581f4fda95ff241994e12e6d86
Samuel <samukedo@gmail.com>
2020-03-17 20:34:24 +0100

    perfect to the link

--- src/containers/layout.js
+++ src/containers/layout.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { NavLink, Route } from 'react-router-dom';
+import { NavLink, Route, withRouter } from 'react-router-dom';
 
-import Footer from "../components/footer"
+import Footer from '../components/footer';
 import '../sass/main.scss';
 import Home from '../pages/home';
 import '../utils/fontawesome';
@@ -10,6 +10,19 @@ import LanguagePack from '../components/languagePack';
 
 const Layout = props => {
 	const [contentIsVisible, setContentIsVisible] = useState(false);
+
+	useEffect(() => {
+		const loc = props.location.pathname;
+		if (
+			loc === '/contact' ||
+			loc === '/trusted-me' ||
+			loc === '/about-me' ||
+			loc === '/offer' ||
+			loc === '/realisations'
+		) {
+			setContentIsVisible(true);
+		}
+	});
 
 	const openContentHandler = () => {
 		setContentIsVisible(true);
@@ -47,4 +60,4 @@ const Layout = props => {
 		</main>
 	);
 };
-export default Layout;
+export default withRouter(Layout);
--- src/pages/home.js
+++ src/pages/home.js
@@ -5,15 +5,17 @@ import { NavLink } from 'react-router-dom';
 import Logo from '../components/logo';
 import HomeMenu from '../components/homeMenu';
 
-const Home = props => (
-	<section className={props.contentIsVisible ? 'home home--right' : 'home'}>
-		<div className="home-container">
-			<NavLink to="/" exact onClick={props.closeContent}>
-				<Logo />
-			</NavLink>
-			<HomeMenu openContent={props.openContent} closeContent={props.closeContent} />
-		</div>
-	</section>
-);
+const Home = props => {
+	return (
+		<section className={props.contentIsVisible ? 'home home--right' : 'home'}>
+			<div className="home-container">
+				<NavLink to="/" exact onClick={props.closeContent}>
+					<Logo />
+				</NavLink>
+				<HomeMenu openContent={props.openContent} closeContent={props.closeContent} />
+			</div>
+		</section>
+	);
+};
 
 export default Home;
